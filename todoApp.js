a2cdc39b526761d4bbfd531322f2b5797e9a17e5
aniebodavid <aniebovictor001@gmail.com>
2022-03-06 02:39:59 -0800

    Add javaScript file

--- todoApp.js
+++ todoApp.js
@@ -1,3 +1,14 @@
 // display checkicon
 // remove checkmark border
 // set checkmark background color
+class Todo {
+  constructor(taskItem) {
+    this.taskItem = taskItem
+  }
+}
+
+class TaskUI {}
+
+// Event Listeners
+
+// Event Listener for Delete
