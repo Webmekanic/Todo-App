afd5f2013bfc3e90178d84144f2ebbc4d1016bdc
aniebodavid <aniebovictor001@gmail.com>
2022-03-07 22:59:33 -0800

    Add todo list

--- todoApp.js
+++ todoApp.js
@@ -7,12 +7,21 @@ class Todo {
   }
 }
 
-class TaskUI {}
+class TaskUI {
+  addTask(todo) {
+    const taskList = document.querySelector(".taskList")
+
+    // create element for todo
+  }
+}
 
 // Event Listeners
 document.querySelector("#taskForm").addEventListener("keypress", (e) => {
+  const taskInput = document.querySelector("#enterTask").value
+
+  const todo = new Todo(taskInput)
+  const taskui = new TaskUI()
   if (e.key === "Enter") {
-    const taskInput = document.querySelector("#enterTask").value
     console.log(taskInput)
     e.preventDefault()
   }
