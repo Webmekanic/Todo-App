b0c8371ffbe0ad0ba31153d4e602836185a1fb94
aniebodavid <aniebovictor001@gmail.com>
2022-03-10 06:30:29 -0800

    Add checked functionality

--- todoApp.js
+++ todoApp.js
@@ -1,6 +1,3 @@
-// display checkicon
-// remove checkmark border
-// set checkmark background color
 class Todo {
   constructor(taskInput) {
     this.taskInput = taskInput
@@ -10,7 +7,6 @@ class Todo {
 class TaskUI {
   addTask(todo) {
     const taskList = document.querySelector(".taskList")
-
     // create element for todo
     const li = document.createElement("li")
     li.className = "tasklist-item"
@@ -22,15 +18,36 @@ class TaskUI {
     section.appendChild(img)
     li.appendChild(section)
     const pTag = document.createElement("p")
-    pTag.id = "task"
+    pTag.className = "task"
     pTag.innerHTML = `${todo.taskInput}`
     li.appendChild(pTag)
     const deleteImg = document.createElement("img")
     deleteImg.className = "deleteItem"
     deleteImg.setAttribute("src", "./images/icon-cross.svg")
     li.appendChild(deleteImg)
-
     taskList.appendChild(li)
+  }
+
+  checkedTask(target) {
+    console.log(target)
+    if (target.classList.contains("checkMark")) {
+      target.classList.add("active")
+      target.lastElementChild.classList.add("active")
+      target.nextElementSibling.classList.add("strike")
+    }
+    if (target.classList.contains("checkIcon")) {
+      target.classList.remove("active")
+      target.parentElement.classList.remove("active")
+      console.log(target)
+      target.parentElement.nextElementSibling.classList.remove("strike")
+    }
+  }
+
+  // Delete Task
+  deleteTask(target) {
+    if (target.className === "deleteItem") {
+      target.parentElement.remove()
+    }
   }
 }
 
@@ -42,9 +59,19 @@ document.querySelector("#taskForm").addEventListener("keypress", (e) => {
   const taskui = new TaskUI()
   if (e.key === "Enter") {
     taskui.addTask(todo)
-    // console.log(taskInput)
     e.preventDefault()
   }
 })
 
+document.querySelector(".taskList").addEventListener("click", (e) => {
+  const taskui = new TaskUI()
+  taskui.deleteTask(e.target)
+  e.preventDefault()
+})
+
 // Event Listener for Delete
+document.querySelector(".taskList").addEventListener("click", (e) => {
+  const taskui = new TaskUI()
+  taskui.checkedTask(e.target)
+  e.preventDefault()
+})
